cff414341643aebb5d2e6bff0f5ac0db3c66d34c
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2023-06-04 19:49:15 +0100

    Added Surefire plugin for integrated testing.

--- src/main/java/MathUtils.java
+++ src/main/java/MathUtils.java
@@ -1,8 +1,90 @@
+import java.util.Arrays;
+
 public class MathUtils {
 
 
-    public int add(int a, int b){
+    public int add(int a, int b) {
         return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+    public int divide(int a, int b) {
+        return a / b;
+    }
+
+    public double computeCircleArea(double radius) {
+
+
+        return Math.PI * radius * radius;
+    }
+
+//    int[] arr = {1,2,3,4,5,6,7,8,9,10};
+//    int[] arr = new int[3];
+
+    public static int binarySearch(int[] arr, int target) {
+//        arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] == target) {
+                System.out.println(arr[mid]);
+                return arr[mid];
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static int[] mergesort(int[] arr) {
+        if (arr.length <= 1) {
+            return arr;
+        }
+
+        int mid = arr.length / 2;
+        int[] left = mergesort(Arrays.copyOfRange(arr, 0, mid));
+        int[] right = mergesort(Arrays.copyOfRange(arr, mid, arr.length));
+        return merge(left, right);
+    }
+
+    public static int[] merge(int[] left, int[] right) {
+        int[] merged = new int[left.length + right.length];
+        int i = 0, j = 0, k = 0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] < right[j]) {
+                merged[k++] = left[i++];
+            } else {
+                merged[k++] = right[j++];
+            }
+        }
+
+        while (i < left.length) {
+            merged[k++] = left[i++];
+        }
+        while (j < right.length) {
+            merged[k++] = right[j++];
+        }
+//        System.out.println(merged.de());
+        System.out.println(Arrays.toString(merged));
+        return merged;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+        binarySearch(arr, 90);
+//        int[] array = {10, 70, 50, 66, 20, 89, 32, 30, 40, 87, 50, 675, 60, 70, 329, 80, 76, 87, 90, 456, 100};
+        int[] array = {675, 60, 70, 329, 80};
+        mergesort(array);
     }
 
 
--- src/test/java/MathUtilsTest.java
+++ src/test/java/MathUtilsTest.java
@@ -5,12 +5,33 @@ import static org.junit.jupiter.api.Assertions.*;
 class MathUtilsTest {
 
     @Test
-    void add() {
+    void testAdd() {
         MathUtils mathUtils = new MathUtils();
-        int expected = 1;
+        int expected = 2;
         int actual = mathUtils.add(1,1);
 
         assertEquals(expected, actual, "The add method should add two numbers");
+
+    }
+
+    @Test
+    void testComputeCircleRadius(){
+        MathUtils mathUtils = new MathUtils();
+        assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "Should return circle area");
+    }
+
+    @Test
+    void subtract() {
+      }
+
+    @Test
+    void multiply() {
+    }
+
+    @Test
+    void divide() {
+        MathUtils mathUtils = new MathUtils();
+        assertThrows(ArithmeticException.class, ()-> mathUtils.divide(1,0), "Divide by 0. Should throw exception.");
 
     }
 }
